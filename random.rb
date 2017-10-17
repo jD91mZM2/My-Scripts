577c1737771c92e23b6d492b196473f7a388bbac
jD91mZM2 <me@krake.one>
2017-10-17 08:56:08 +0200

    random.rb: Take length from command line

--- random.rb
+++ random.rb
@@ -1,16 +1,21 @@
-print "Length: ";
-len = gets.chomp;
+len = nil
+if ARGV.length < 1
+    print "Length: "
+    len = gets.chomp
+else
+    len = ARGV[0]
+end
 
 begin
-	ilen = Integer(len)
+    ilen = Integer(len)
 rescue ArgumentError
-	puts "Not a number";
-	exit;
+    puts "Not a number"
+    exit;
 end
 
-dic = "abcdefghiklmnopqrstuvwxyzABCDEFGHIKLMNOPQRSTUVWXYZ0123456789";
-result = "";
+dic = "abcdefghiklmnopqrstuvwxyzABCDEFGHIKLMNOPQRSTUVWXYZ0123456789"
+result = ""
 for i in 1..ilen
-	result += dic[rand(dic.length)];
+    result += dic[rand(dic.length)]
 end
-puts result;
+puts result
